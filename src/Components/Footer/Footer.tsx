5948e0c39cf55a4670d89ca88cf03636658da9c6
이서린 <iseolin76@gmail.com>
2021-02-27 20:59:20 +0900

    [Create] Footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import * as S from "./styled";
+import * as I from "../../Asset/SVG/index";
+
+const Footer: React.FC = () => {
+  return (
+    <S.Footer>
+      <I.Logo />
+    </S.Footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Footer/styled.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Footer = styled.div`
+  margin: 0 10%;
+  border-top: 1px solid #434c9c;
+  svg {
+    margin-top: 40px;
+  }
+`;
