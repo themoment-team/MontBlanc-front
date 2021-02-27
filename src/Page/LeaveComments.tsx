9e00d8c4dfe588062a10e8f8e398b1c344f78e15
이서린 <iseolin76@gmail.com>
2021-02-27 21:02:55 +0900

    [Update] Router

---
+++ src/Page/LeaveComments.tsx
@@ -0,0 +1,12 @@
+import { Header, Footer } from "../Components/index";
+
+const LeaveComments: React.FC = () => {
+  return (
+    <>
+      <Header state="student" />
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default LeaveComments;
---
+++ src/Page/index.tsx
@@ -0,0 +1,1 @@
+export { default as LeaveComments } from "./LeaveComments";
