0b3d1965852a38411800119941a0833f50c13188
이서린 <iseolin76@gmail.com>
2021-03-31 20:57:41 +0900

    about 페이지 초석만 닦아놓았습니다

---
+++ src/Page/about.tsx
@@ -0,0 +1,13 @@
+import { Header, Footer } from "../Components";
+
+const about: React.FC = () => {
+  return (
+    <>
+      <Header state="student" />
+      <div>캠페인 자세히 보기</div>
+      <Footer />
+    </>
+  );
+};
+
+export default about;
