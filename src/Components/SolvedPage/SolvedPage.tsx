bf7af437dfa0ec50defb88180e6fbe81be4187dc
이서린 <iseolin76@gmail.com>
2021-04-01 09:27:24 +0900

    [Delete] SolvedPage

--- src/Components/SolvedPage/SolvedPage.tsx
+++
@@ -1,8 +0,0 @@
-import Solved from "../../Page/Solved";
-import PageExplanation from "../PageExplanation/PageExplanation";
-
-const SolvedPage: React.FC = () => {
-  return <div>웅어</div>;
-};
-
-export default SolvedPage;
