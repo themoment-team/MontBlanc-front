cacdfb578fa4ada0ddebc0d31fc3fc1d59be8d30
Goolgae <sunwoo0706@icloud.com>
2021-04-02 15:08:23 +0900

    [Update] Add issue elem in Top10Page

--- src/Components/Top10Page/Top10Page.tsx
+++ src/Components/Top10Page/Top10Page.tsx
@@ -2,8 +2,9 @@ import { PageExplanation } from "../PageExplanation";
 import { Link } from "react-router-dom";
 import { LeftBox } from "../../Styles/LeftBox";
 import styled from "styled-components";
+import * as S from "./styled";
 
-const heading: string[] = ["학교가 불편한 순간 TOP 10"];
+const heading: string[] = ["학교가 불편한 순간", "TOP 10"];
 const explanation: string[] = [
   "공감이 되는 의견이 있다면 좋아요를 눌러주세요.",
   "좀 더 적극적으로 개선할 수 있도록 노력하겠습니다.",
@@ -11,12 +12,31 @@ const explanation: string[] = [
 
 const Top10Page: React.FC = () => {
   return (
-    <LeftBox>
-      <PageExplanation heading={heading} explanation={explanation} />
-      <Btn>
-        <Link to="/Leave_opinion">의견 남기기</Link>
-      </Btn>
-    </LeftBox>
+    <S.TopTenWrapper>
+      <LeftBox>
+        <PageExplanation heading={heading} explanation={explanation} />
+        <Btn>
+          <Link to="/Leave_opinion">의견 남기기</Link>
+        </Btn>
+      </LeftBox>
+      <S.RightBox>
+        <S.TenIssues>
+          <span>
+            <span>
+              {1}위
+            </span>
+            <article>
+              {}전공 동아리 시간을 늘려주세요요오옷ㅇㅅ오ㅗ서어소요성섯용서엿ㅅㅇㅅ옷욧ㅇ소
+            </article>
+          </span>
+          <span>
+            <button>
+              답변{"보기"}
+            </button>
+          </span>
+        </S.TenIssues>
+      </S.RightBox>
+    </S.TopTenWrapper>
   );
 };
 
