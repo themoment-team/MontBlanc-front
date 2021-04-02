949a86fb6552dd361c14fc45d73fefb9981e2eaf
이서린 <iseolin76@gmail.com>
2021-04-02 15:11:47 +0900

    [Update] Header -> fixed

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -14,25 +14,27 @@ const Header: React.FC<HeaderProps> = (p: HeaderProps) => {
           <I.Logo />
           <span>학교가 불편한 순간</span>
         </div>
-        <ul>
+        <S.HeaderNav>
           <Link to="/Leave_opinion">답변달기</Link>
           <Link to="/improvment">실제 개선 사례 작성</Link>
-        </ul>
+        </S.HeaderNav>
       </S.Header>
     );
   } else {
     return (
       <S.Header>
         <div>
-          <I.Logo />
-          <span>학교가 불편한 순간</span>
+          <S.Logo>
+            <I.Logo />
+          </S.Logo>
+          <S.LogoText>학교가 불편한 순간</S.LogoText>
         </div>
-        <ul>
+        <S.HeaderNav>
           <Link to="/Leave_opinion">의견 남기기</Link>
           <Link to="/v1/uncomfortable">Top 10</Link>
           <Link to="/improvment">실제 개선 사례</Link>
           <Link to="/about">캠페인 자세히 보기</Link>
-        </ul>
+        </S.HeaderNav>
       </S.Header>
     );
   }
