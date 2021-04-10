157a76111c77d1dbaa5f065537c86b458f36fa42
Goolgae <sunwoo0706@icloud.com>
2021-04-10 23:42:27 +0900

    Add modal container

--- src/Modal/modalContainer.tsx
+++ src/Modal/modalContainer.tsx
@@ -1,51 +1,65 @@
-import React, { Component, createContext } from "react";
+import React, { createContext, useState, useCallback, useContext } from "react";
 
-const ModalContext = createContext({
-  component: null,
-  props: {},
-  showModal: (component: any, props = {}) => {},
-  hideModal: () => {},
-});
+import * as modals from "../components/Modals";
 
-const Consumer = ModalContext.Consumer;
-
-export class ModalProvider extends React.Component {
-  showModal = (component: any, props = {}) => {
-    this.setState({
-      component,
-      props,
-    });
-  };
-
-  hideModal = () =>
-    this.setState({
-      component: null,
-      props: {},
-    });
-
-  state = {
-    component: null,
-    props: {},
-    showModal: this.showModal,
-    hideModal: this.hideModal,
-  };
-
-  render() {
-    return (
-      <ModalContext.Provider value={this.state}>
-        {this.props.children}
-      </ModalContext.Provider>
-    );
-  }
-}
-
-const Modal = (props: any) => {
-  const Content = props.content;
-  return <Content />;
+export type ModalProps = {
+  close: () => void;
 };
 
-const CloseButton = () => (
-  <Consumer>
-    {({ hideModal }) => <button onClick={hideModal}>닫기</button>}
-  </Consumer>
-);
+type Modal = {
+  name: keyof typeof modals;
+  props: {
+    [key in string]: any;
+  };
+};
+
+type ModalContextValues = {
+  open: <T extends Modal["name"]>(
+    name: T,
+    props: Omit<
+      React.ComponentProps<typeof modals[T]>,
+      keyof ModalProps | "children"
+    >
+  ) => void;
+};
+
+const ModalContext = createContext<ModalContextValues | null>(null);
+
+export const ModalContextProvider: React.FC = ({ children }) => {
+  const [openedModal, setOpenedModal] = useState<Modal | null>(null);
+
+  const open: ModalContextValues["open"] = useCallback((name, props) => {
+    setOpenedModal({ name, props });
+  }, []);
+
+  const close = useCallback(() => {
+    setOpenedModal(null);
+  }, []);
+
+  return (
+    <ModalContext.Provider value={{ open }}>
+      {children}
+      {openedModal && <ModalRenderer close={close} modal={openedModal} />}
+    </ModalContext.Provider>
+  );
+};
+
+const ModalRenderer: React.FC<ModalProps & { modal: Modal }> = ({
+  close,
+  modal,
+}) => {
+  const Modal = modals[modal.name];
+  return <Modal close={close} {...(modal.props as any)} />;
+};
+
+// Modal Context의 Value들을 사용할 수 있는 hook
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+
+  if (!context) {
+    throw new Error("context가 제공되어야 합니다.");
+  }
+
+  return context;
+};
