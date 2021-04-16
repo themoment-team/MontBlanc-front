4686f1a7b35fef9a9f79d982108f363bf96a3b03
이서린 <iseolin76@gmail.com>
2021-04-16 10:08:00 +0900

    Create LeaveCommentModal

---
+++ src/Components/Modals/ModalList/LeaveCommentModal/LeaveCommentModal.tsx
@@ -0,0 +1,18 @@
+import { ModalContainer } from "../../ModalContainer";
+import { ModalProps } from "../../../../Context/Modal";
+import * as S from "./style";
+
+const LeaveCommentModal: React.FC<ModalProps> = ({ close }) => {
+  return (
+    <ModalContainer close={close} width={1000} height={600}>
+      <S.ModalWrapper>
+        <S.H1>답변 달기</S.H1>
+        <S.Input placeholder="제목을 입력해주세요." />
+        <S.TextArea placeholder="내용을 입력해주세요." />
+        <S.SaveBtn>저 장</S.SaveBtn>
+      </S.ModalWrapper>
+    </ModalContainer>
+  );
+};
+
+export default LeaveCommentModal;
--- src/Components/Modals/index.tsx
+++ src/Components/Modals/index.tsx
@@ -1,2 +1,3 @@
 export { default as LoginModal } from "./ModalList/LoginModal/LoginModalPresenter";
 export { default as ImprovmentModal } from "./ModalList/ImprovmentModal/ImprovmentModalPresenter";
+export { default as LeaveCommentModal } from "./ModalList/LeaveCommentModal/LeaveCommentModal";
