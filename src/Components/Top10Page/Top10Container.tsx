d6c36ddfc0c36f423da12efc8a32e45de4ec0313
Goolgae <sunwoo0706@icloud.com>
2021-04-20 09:41:04 +0900

    Add Top10 dummy data

---
+++ src/Components/Top10Page/Top10Container.tsx
@@ -0,0 +1,12 @@
+export const Top10List = [
+    {id: 1, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 2, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 3, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 4, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 5, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 6, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 7, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 8, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 9, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+    {id: 10, content: "전공동아리 시간 좀 늘려주시겠습니까. 교장쌤", option: "보기"},
+]
