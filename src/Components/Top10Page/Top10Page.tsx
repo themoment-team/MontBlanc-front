3315fd07275ab038667ab6e4c1db95d809e68fc7
이서린 <iseolin76@gmail.com>
2021-04-25 15:25:19 +0900

    Delete 쓸모없는 임포트

--- src/Components/Top10Page/Top10Page.tsx
+++ src/Components/Top10Page/Top10Page.tsx
@@ -1,18 +1,16 @@
+import { useEffect, useState } from "react";
 import { PageExplanation } from "../PageExplanation";
-import { Link } from "react-router-dom";
-import { LeftBox } from "../../Constants/GlobalStyle/LeftBox"
-import { BASE_URL } from "../../Constants/Config.json"
+import { LeftBox, ApiResult } from "../../Constants/GlobalStyle/Detail";
 import axios from "axios";
 import GoodBtn from "../GoodBtn/GoodBtnPresenter";
 import * as S from "./styled";
-import { useEffect, useState } from "react";
 
 // list 인터페이스
 interface list {
   map(arg0: (top10: any) => JSX.Element): import("react").ReactNode;
-  boardIdx : number;
-  content : string;
-  goods : number;
+  boardIdx: number;
+  content: string;
+  goods: number;
 }
 
 const heading: string[] = ["학교가 불편한 순간", "TOP 10"];
@@ -31,13 +29,15 @@ const Top10Page: React.FC = () => {
       setList(null);
       setLoading(true);
       setError(null);
-      const response = await axios.get("http://192.168.0.4:5000/v1/uncomfortable");
-      setList(response.data.list)
-    } catch(e) {
+      const response = await axios.get(
+        "http://192.168.0.4:5000/v1/uncomfortable"
+      );
+      setList(response.data.list);
+    } catch (e) {
       setError(e);
     }
     setLoading(false);
-  }
+  };
 
   useEffect(() => {
     fetchList();
@@ -45,9 +45,9 @@ const Top10Page: React.FC = () => {
 
   // return
 
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다.</div>
-  if (!list) return <div>안녕하셍요</div>;
+  if (loading) return <ApiResult>로딩중..</ApiResult>;
+  if (error) return <ApiResult>에러가 발생했습니다.</ApiResult>;
+  if (!list) return <ApiResult>리스트가 없슴다.</ApiResult>;
 
   return (
     <S.TopTenWrapper>
@@ -58,17 +58,15 @@ const Top10Page: React.FC = () => {
         </S.Btn>
       </LeftBox>
       <S.RightBox>
-        {list.map(top10 => (
+        {list.map((top10) => (
           <S.TenIssues>
             <span>
               <span>{top10.boardIdx}위</span>
-              <article>
-                {top10.content}
-              </article>
+              <article>{top10.content}</article>
             </span>
             <span>
               <button>답변보기</button>
-              <GoodBtn Background={false} Goods={top10.goods}/>
+              <GoodBtn Background={false} Goods={top10.goods} />
             </span>
           </S.TenIssues>
         ))}
