39a124bcd394b33f1291a5fd6d5d5136b8716722
이서린 <iseolin76@gmail.com>
2021-04-25 15:24:19 +0900

    Update LeftBox -> Detail

---
+++ src/Constants/GlobalStyle/Detail.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ApiResult = styled.h1`
+  margin-top: 150px;
+  text-align: center;
+  color: #434c9c;
+`;
+
+export const LeftBox = styled.div`
+  width: 45%;
+`;
--- src/Constants/GlobalStyle/LeftBox.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const LeftBox = styled.div`
-  width: 45%;
-`;
