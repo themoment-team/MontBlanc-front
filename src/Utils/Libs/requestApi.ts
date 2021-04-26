efdd5caa6ae52e68f35ae0ecfdf83ef1edd7ad43
이서린 <iseolin76@gmail.com>
2021-04-26 20:24:26 +0900

    Refactor RequestApi

--- src/Utils/Libs/requestApi.ts
+++ src/Utils/Libs/requestApi.ts
@@ -1,74 +1,34 @@
 import axios, { AxiosRequestConfig } from "axios";
-export const methodType = {
-  GET: "get",
-  POST: "post",
-  PUT: "put",
-  DELETE: "delete",
-};
-
 export const ACCESS_TOKEN_NAME = "Authorization";
+import { BASE_URL, BASE_HEADER } from "Constants/Config.json";
 
-// 토큰 없는 파라미터가 필요한 api 요청
-export const requestApiWithBodyWithoutToken = async (p: AxiosRequestConfig) => {
-  try {
-    const res = await axios({
-      method: p.method,
-      url: p.baseURL + p.url,
-      paramsSerializer: p.paramsSerializer,
-      data: {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type,Authorization",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-          "Access-Control-Allow-Credentials": "true",
-          ...p.headers,
-        },
-      },
-    });
-    return res;
-  } catch (error) {
-    throw error;
-  }
-};
-//토큰과 파라미터 없는 api 요청
-export const requestApiWithoutBodyWithoutToken = async (
-  p: AxiosRequestConfig
+export interface hasPrameterAndToken {
+  hasToken: boolean;
+  hasParameter: boolean;
+}
+
+const RequestApi = async (
+  p: AxiosRequestConfig,
+  condition: hasPrameterAndToken = { hasToken: true, hasParameter: true }
 ) => {
+  const headers = p.headers;
+  const accessToken =
+    condition.hasToken === true
+      ? localStorage.getItem("themoment_token")
+      : undefined;
+  const parameter =
+    condition.hasParameter === true ? p.paramsSerializer : undefined;
   try {
     const res = await axios({
       method: p.method,
-      url: p.baseURL + p.url,
+      baseURL: BASE_URL,
+      url: p.url,
+      paramsSerializer: parameter,
       data: {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type,Authorization",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-          "Access-Control-Allow-Credentials": "true",
-          ...p.headers,
-        },
-      },
-    });
-    return res;
-  } catch (error) {
-    throw error;
-  }
-};
-//토큰이 필요한 파라미터 없는 api 요청
-export const requestApiWithoutBodyWithToken = async (p: AxiosRequestConfig) => {
-  try {
-    const accessToken = window.localStorage.getItem("themoment_token");
-    const res = await axios({
-      method: p.method,
-      url: p.baseURL + p.url,
-      data: {
-        headers: {
-          [ACCESS_TOKEN_NAME]: `${accessToken}`,
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type,Authorization",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-          "Access-Control-Allow-Credentials": "true",
-          ...p.headers,
-        },
+        headers: Object.assign(BASE_HEADER, headers, {
+          [ACCESS_TOKEN_NAME]: accessToken,
+        }),
+        body: p.data,
       },
     });
     return res;
@@ -77,27 +37,4 @@ export const requestApiWithoutBodyWithToken = async (p: AxiosRequestConfig) => {
   }
 };
 
-//토큰과 파라미터가 모두 필요한 API 요청
-export const requestApiWithBodyWithToken = async (p: AxiosRequestConfig) => {
-  try {
-    const accessToken = window.localStorage.getItem("themoment_token");
-    const res = await axios({
-      method: p.method,
-      url: p.baseURL + p.url,
-      paramsSerializer: p.paramsSerializer,
-      data: {
-        headers: {
-          [ACCESS_TOKEN_NAME]: `${accessToken}`,
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type,Authorization",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS",
-          "Access-Control-Allow-Credentials": "true",
-          ...p.headers,
-        },
-      },
-    });
-    return res;
-  } catch (error) {
-    throw error;
-  }
-};
+export default RequestApi;
