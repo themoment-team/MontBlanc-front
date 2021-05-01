cfc19d9922df48ed6e6bc28194180999d96e2157
Goolgae <sunwoo0706@icloud.com>
2021-05-01 15:26:01 +0900

    Add token recoil state

--- src/Utils/Libs/requestApi.ts
+++ src/Utils/Libs/requestApi.ts
@@ -1,5 +1,7 @@
 import { BASE_URL, BASE_HEADER } from "Constants/Config.json";
 import axios, { AxiosRequestConfig } from "axios";
+import { useRecoilState } from "recoil";
+import * as A from "../../Atom/AtomPresenter";
 
 export interface hasToken {
   hasToken?: boolean;
@@ -9,11 +11,11 @@ const RequestApi = async (
   p: AxiosRequestConfig,
   condition: hasToken = { hasToken: false }
 ) => {
-  let accessToken: string | null = null;
-  let refreshToken: string | null = null;
+  const [accessToken, setAccessToken] = useRecoilState(A.AccessToken);
+  const [refreshToken, setRefreshToken] = useRecoilState(A.RefreshToken);
   if (condition.hasToken) {
-    accessToken = localStorage.getItem("themoment_token");
-    refreshToken = localStorage.getItem("themoment_token_refresh");
+    setAccessToken(localStorage.getItem("themoment_token"));
+    setRefreshToken(localStorage.getItem("themoment_token_refresh"));
   }
   try {
     let header = Object.assign(BASE_HEADER, p.headers);
