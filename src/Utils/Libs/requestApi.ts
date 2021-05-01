4e84e0a649faae9bd3de8943b1801e31d18219da
Goolgae <sunwoo0706@icloud.com>
2021-05-01 16:12:57 +0900

    Rebase

--- src/Utils/Libs/requestApi.ts
+++ src/Utils/Libs/requestApi.ts
@@ -1,7 +1,5 @@
 import { BASE_URL, BASE_HEADER } from "Constants/Config.json";
 import axios, { AxiosRequestConfig } from "axios";
-import { useRecoilState } from "recoil";
-import * as A from "../../Atom/AtomPresenter";
 
 export interface hasToken {
   hasToken?: boolean;
@@ -11,11 +9,11 @@ const RequestApi = async (
   p: AxiosRequestConfig,
   condition: hasToken = { hasToken: false }
 ) => {
-  const [accessToken, setAccessToken] = useRecoilState(A.AccessToken);
-  const [refreshToken, setRefreshToken] = useRecoilState(A.RefreshToken);
+  let accessToken: string | null = null;
+  let refreshToken: string | null = null;
   if (condition.hasToken) {
-    setAccessToken(localStorage.getItem("themoment_token"));
-    setRefreshToken(localStorage.getItem("themoment_token_refresh"));
+    accessToken = localStorage.getItem("themoment_token");
+    refreshToken = localStorage.getItem("themoment_token_refresh");
   }
   try {
     let header = Object.assign(BASE_HEADER, p.headers);
