708925c7a1711399006505015ecc20ea34ffb4ad
Goolgae <sunwoo0706@icloud.com>
2021-05-01 14:48:28 +0900

    Add recoil root

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import * as P from "./Page/index";
 import GlobalStyle from "./Constants/GlobalStyle/GlobalStyles";
+import { RecoilRoot } from 'recoil';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <GlobalStyle />
     <Switch>
-      <Route path="/about" component={P.about} />
-      <Route path="/improvment" component={P.improvment} />
-      <Route path="/topten" component={P.Top10} />
-      <Route path="/Leave_opinion" component={P.LeaveComments} />
-      <Route path="/" component={P.Start} />
-      <Redirect path="*" to="/" />
+      <RecoilRoot>
+        <Route path="/about" component={P.about} />
+        <Route path="/improvment" component={P.improvment} />
+        <Route path="/topten" component={P.Top10} />
+        <Route path="/Leave_opinion" component={P.LeaveComments} />
+        <Route path="/" component={P.Start} />
+        <Redirect path="*" to="/" />
+      </RecoilRoot>
     </Switch>
   </BrowserRouter>
 );
