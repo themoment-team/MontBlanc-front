82b6c00f4020b98a886f3b48ab5db029d3227cb9
Goolgae <sunwoo0706@icloud.com>
2021-05-02 14:11:23 +0900

    Fix if -> Ternary operator

--- src/Components/Common/Header/Header.tsx
+++ src/Components/Common/Header/Header.tsx
@@ -7,40 +7,29 @@ import { HasAdminToken } from "../../../Atom";
 const Header: React.FC = () => {
   const logged = useRecoilValue(HasAdminToken);
   
-  if (logged === true) {
-    return (
-      <S.Header>
-        <div>
-          <S.Logo>
-            <I.Logo />
-          </S.Logo>
-          <S.LogoText>학교가 불편한 순간</S.LogoText>
-        </div>
+  return (
+    <S.Header>
+      <div>
+        <S.Logo>
+          <I.Logo />
+        </S.Logo>
+        <S.LogoText>학교가 불편한 순간</S.LogoText>
+      </div>
+      { logged === true ?
         <S.HeaderNav>
           <Link to="/Leave_opinion">답변달기</Link>
           <Link to="/improvment">실제 개선 사례 작성</Link>
-          <a>로그아웃</a>
-        </S.HeaderNav>
-      </S.Header>
-    );
-  } else {
-    return (
-      <S.Header>
-        <div>
-          <S.Logo>
-            <I.Logo />
-          </S.Logo>
-          <S.LogoText>학교가 불편한 순간</S.LogoText>
-        </div>
+          <button>로그아웃</button>
+        </S.HeaderNav> :
         <S.HeaderNav>
           <Link to="/Leave_opinion">의견 남기기</Link>
           <Link to="/topten">Top 10</Link>
           <Link to="/improvment">실제 개선 사례</Link>
           <Link to="/about">캠페인 자세히 보기</Link>
         </S.HeaderNav>
-      </S.Header>
-    );
-  }
+      }
+    </S.Header>
+  );
 };
 
 export default Header;
