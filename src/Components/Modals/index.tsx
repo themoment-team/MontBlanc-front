4af6fc718bd58d256936fce083c799287ed0c627
이서린 <iseolin76@gmail.com>
2021-05-04 23:37:14 +0900

    Refactor EditModal -> EditModalPresenter

--- src/Components/Modals/index.tsx
+++ src/Components/Modals/index.tsx
@@ -1,3 +1,3 @@
 export { default as LoginModal } from "./ModalList/LoginModal/LoginModalPresenter";
 export { default as ViewModal } from "./ModalList/ViewModal/ViewModalPresenter";
-export { default as EditModal } from "./ModalList/EditModal/EditModal";
+export { default as EditModal } from "./ModalList/EditModal/EditModalPresenter";
