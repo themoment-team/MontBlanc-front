1d6126f1c607f4c9f51c2733d83a3c27edde5ac4
iseolin76 <iseolin76@gmail.com>
2021-05-31 03:58:11 +0900

    [Create] 반응형 기준 모듈

---
+++ src/Constants/reactive.ts
@@ -0,0 +1,13 @@
+const size = {
+  mobile: "640px",
+  tablet: "960px",
+  laptop: "1080px",
+};
+
+const device = {
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+};
+
+export default device;
