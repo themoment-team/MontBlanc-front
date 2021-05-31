b72f2a611445ddae6dbd1cd7f3f8f25df3c34e6d
iseolin76 <iseolin76@gmail.com>
2021-06-01 01:43:07 +0900

    헤더 반응형

--- src/Components/Common/Header/styled.tsx
+++ src/Components/Common/Header/styled.tsx
@@ -1,18 +1,40 @@
 import styled from "styled-components";
 import device from "Constants/reactive";
 
+interface NavBar {
+  active: boolean;
+}
+
 export const Header = styled.header`
   position: fixed;
   top: 0;
   display: flex;
   justify-content: space-between;
+  backdrop-filter: blur(5px);
   padding: 23px 15vw;
   width: 70vw;
-  background: white;
   z-index: 1;
+  background: white;
   @media ${device.mobile} {
     width: 90vw;
     padding: 23px 5vw;
+  }
+  & > div {
+    color: #434c9c;
+    font-weight: 600;
+    font-size: 22px;
+    position: relative;
+    top: -10px;
+    margin: 21px;
+  }
+`;
+
+export const Logo = styled.h1`
+  display: inline-block;
+  border-right: 2px solid #434c9c;
+  padding-right: 21px;
+  @media screen and (max-width: 1400px) {
+    border: none;
   }
 `;
 
@@ -23,24 +45,54 @@ export const LogoText = styled.span`
   margin-left: 21px;
   position: relative;
   top: -13px;
+  @media ${device.mobile} {
+    display: none;
+  }
 `;
 
-export const Logo = styled.h1`
-  display: inline-block;
-  border-right: 1px solid #434c9c;
-  padding-right: 21px;
+export const BarWrapper = styled.div`
+  @media screen and (min-width: 1200px) {
+    display: none;
+  }
+  & > svg {
+    z-index: 99;
+    font-size: 32px;
+    color: #434c9c;
+    position: absolute;
+    top: 32px;
+    right: 9.5%;
+  }
 `;
 
-export const HeaderNav = styled.nav`
-  margin-top: 30px;
-  width: 50%;
-  list-style: none;
+export const HeaderNav = styled.nav<NavBar>`
   display: flex;
   justify-content: space-between;
-  button {
+  @media screen and (max-width: 1200px) {
+    flex-direction: column;
+    align-items: flex-start;
+    display: ${(props) => (props.active ? "none" : "flex")};
+  }
+  & > a {
+    display: inline-block;
+    margin: 36px;
+    @media screen and (max-width: 1200px) {
+      margin: 24px 0;
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+  }
+  & > button {
+    display: inline-block;
     padding: 0;
-    color: #434c9c;
+    margin: 36px;
     background: none;
-    font-size: 16px;
+    text-decoration: none;
+    color: #434c9c;
+    font-size: 1rem;
+    @media screen and (max-width: 1200px) {
+      margin: 24px 0;
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
   }
 `;
