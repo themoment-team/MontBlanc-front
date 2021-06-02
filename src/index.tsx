78c49f5173d72180dd20f5a8a2ad228bbb5fb4f2
iseolin76 <iseolin76@gmail.com>
2021-06-02 14:41:54 +0900

    [UPdate] 쓸모없는 기본 주석 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { ModalContextProvider } from "./Context/Modal";
-import { RecoilRoot } from 'recoil';
-import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from "recoil";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <RecoilRoot>
@@ -14,10 +14,7 @@ ReactDOM.render(
       </React.StrictMode>
     </ModalContextProvider>
   </RecoilRoot>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
