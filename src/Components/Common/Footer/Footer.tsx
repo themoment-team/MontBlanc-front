eb85bf40f5605748ee0bf98910ac1256e4671215
Goolgae <sunwoo0706@icloud.com>
2021-06-07 01:19:14 +0900

    Add Footer redesign

--- src/Components/Common/Footer/Footer.tsx
+++ src/Components/Common/Footer/Footer.tsx
@@ -5,6 +5,29 @@ const Footer: React.FC = () => {
   return (
     <S.Footer>
       <I.Logo />
+      <S.LineTextWrapper>
+        <S.LineText>
+          <b>
+            Production:
+          </b>
+          Seorin Lee, Sunwoo Lee, Ji-hwan Jeon, Siwon Jeong, Yewon Jin
+        </S.LineText>
+        <S.LineText>
+          <b>
+            Inquiry and email:
+          </b>
+          official.themoment.team@gmail.com
+        </S.LineText>
+        <S.LineText>
+          <b>
+            Github and issue:
+          </b>
+          https://github.com/theMomentTeam
+        </S.LineText>
+      </S.LineTextWrapper>
+      <S.LineText>
+        © 2021, the_moment_team
+      </S.LineText>
     </S.Footer>
   );
 };
--- src/Components/Common/Footer/styled.tsx
+++ src/Components/Common/Footer/styled.tsx
@@ -12,3 +12,17 @@ export const Footer = styled.div`
     margin: 70px 5vw;
   }
 `;
+
+export const LineTextWrapper = styled.div`
+  margin-top: 27px;
+  margin-bottom: 30px;
+`
+
+export const LineText = styled.div`
+  color: #434c9c;
+  font-size: 12px;
+  b {
+    font-weight: 700;
+    margin-right: 0.2rem;
+  }
+`
