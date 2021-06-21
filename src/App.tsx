7da602b42929de9c9c0e2bd14038b6fd20ce8082
iseolin76 <iseolin76@gmail.com>
2021-06-21 16:09:12 +0900

    [update] Top10 -> Top30

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => (
     <Switch>
       <Route path={Config.LINK.ABOUT} component={P.about} />
       <Route path={Config.LINK.IMPROVMENT} component={P.improvment} />
-      <Route path={Config.LINK.TOP10} component={P.Top10} />
+      <Route path={Config.LINK.RANK} component={P.Top10} />
       <Route path={Config.LINK.COMMENT} component={P.LeaveComments} />
       <Route path={Config.LINK.START} component={P.Start} />
       <Redirect path="*" to={Config.LINK.COMMENT} />
