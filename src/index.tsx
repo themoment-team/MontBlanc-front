407c2fe8e4063c4e52875ce04e4482478973fe82
iseolin76 <iseolin76@gmail.com>
2021-09-28 08:31:40 +0900

    Remove: context modal 의 잔재

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { ModalContextProvider } from "./Context/Modal";
 import { RecoilRoot } from "recoil";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <RecoilRoot>
-    <ModalContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ModalContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </RecoilRoot>,
   document.getElementById("root")
 );
