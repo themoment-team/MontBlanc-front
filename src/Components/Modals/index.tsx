966773c9223936c4e46f7a5213d3598c8e27646c
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-10-08 22:53:51 +0900

    Update Modal index 변경

--- src/Components/Modals/index.tsx
+++ src/Components/Modals/index.tsx
@@ -1,3 +1,3 @@
-export { default as LoginModal } from "./LoginModal/LoginModalPresenter";
-export { default as ViewModal } from "./ViewModal/ViewModalPresenter";
-export { default as EditModal } from "./EditModal/EditModalPresenter";
+export { default as LoginModal } from './LoginModal';
+export { default as ViewModal } from './ViewModal';
+export { default as EditModal } from './EditModal';
