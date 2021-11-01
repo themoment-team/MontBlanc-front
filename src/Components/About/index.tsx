e705a7b6198454f04fe042cde51d9928c3a63a5d
hyeongrok7874 <hyeongrok7874@naver.com>
2021-11-01 16:15:48 +0900

    Remove existed codes

--- src/Components/About/index.tsx
+++ src/Components/About/index.tsx
@@ -1,45 +1,7 @@
-// import { PageExplanation } from "../PageExplanation";
 import AboutCounting from "Components/AboutCounting";
 import * as S from "./style";
-// import Table from "Api/table";
-// import { useEffect, useState } from "react";
-
-// const useAmount = (): [amount: number, date: number] => {
-//   const [amount, setAmount] = useState({ data: 0 });
-//   const [date, setDate] = useState({ data: 0 });
-//   const getAmount = async () => {
-//     return await Table.amountUncomfortable();
-//   };
-
-//   const getDataSinceProjectStart = async () => {
-//     return await Table.dateSinceProjectStart();
-//   };
-
-//   useEffect(() => {
-//     getAmount().then((res) => setAmount(res.data));
-//     getDataSinceProjectStart().then((res) => setDate(res.data));
-//   }, []);
-
-//   return [amount.data, date.data];
-// };
-
-// const useHeading = (): string[][] => {
-//   const [amount, date] = useAmount();
-//   const day: number = date;
-//   const cnt_comment: number = amount;
-
-//   const heading: string[] = [
-//     `${day}일 동안`,
-//     `${cnt_comment}개의 불편함이`,
-//     `모였습니다.`,
-//   ];
-//   const explanation: string[] = [""];
-
-//   return [heading, explanation];
-// };
 
 const AboutPage: React.FC = () => {
-  // const [heading, explanation] = useHeading();
   return (
     <S.AboutBox>
       <AboutCounting></AboutCounting>
