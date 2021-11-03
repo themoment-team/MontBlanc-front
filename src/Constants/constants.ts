bba5ff6137f089bd97f8ff61acb2d22ed238482a
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-11-03 18:01:05 +0900

    Fix NODE_ENV logic

--- src/Constants/constants.ts
+++ src/Constants/constants.ts
@@ -10,6 +10,6 @@ const device = {
   laptop: `(max-width: ${size.laptop})`,
 };
 
-export const BASE_URL: string = process.env.NODE_ENV === "development" ? "http://13.124.55.247:5000/v1/" : "https://server.the-moment.org/v1/";
+export const BASE_URL: string = process.env.NODE_ENV === "production" ? "https://server.the-moment.org/v1/" : "http://13.124.55.247:5000/v1/";
 
 export default device;
