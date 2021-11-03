b6ea39a5ac37755d206b0c52b032ab52ff22e347
yoosion <rsy5487@naver.com>
2021-11-03 23:02:08 +0900

    Add SVG 추가

--- src/Asset/SVG/index.tsx
+++ src/Asset/SVG/index.tsx
@@ -2,6 +2,7 @@ export { default as Logo } from "./Logo";
 export { default as Start_img1 } from "./Start_img1";
 export { default as Start_img2 } from "./Start_img2";
 export { default as Start_img3 } from "./Start_img3";
+export { default as Start_img4 } from "./Start_img4";
 export { default as RightArrow } from "./RightArrow";
 export { default as GoodBtnSvg } from "./GoodBtnSvg";
 export { default as IssueSvg } from "./IssueSvg";
