efad8dabb400ba77e3bb6fdbc991b6b1c01a43bf
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-11-03 17:56:30 +0900

    Update BASE_URL logic

---
+++ src/Constants/constants.ts
@@ -0,0 +1,15 @@
+const size = {
+  mobile: "640px",
+  tablet: "960px",
+  laptop: "1080px",
+};
+
+const device = {
+  mobile: `(max-width: ${size.mobile})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+};
+
+export const BASE_URL: string = process.env.NODE_ENV === "development" ? "http://13.124.55.247:5000/v1/" : "https://server.the-moment.org/v1/";
+
+export default device;
