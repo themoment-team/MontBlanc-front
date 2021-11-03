9003b7af16c62b8b48d0038ea1947dee22b06b41
yoosion <rsy5487@naver.com>
2021-11-03 23:01:33 +0900

    Add SVG 수정

--- src/Asset/SVG/Start_img3.tsx
+++ src/Asset/SVG/Start_img3.tsx
@@ -3,9 +3,7 @@ import React from "react";
 class Start_img3 extends React.Component {
   render() {
     return (
-      <svg width="252" height="2175" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="1820" width="251.613" height="355" rx="12" fill="#FFDEDE"/><rect x=".18" y="292" width="251.613" height="212" rx="12" fill="#CCD1FF"/><rect x=".18" y="533" width="251.613" height="184" rx="12" fill="#C3E2FF"/><rect x=".18" width="251.613" height="263" rx="12" fill="#FFDEDE"/><rect y="1316" width="252" height="475" rx="12" fill="#C3E2FF"/><rect x=".18" y="746" width="251.613" height="256" rx="12" fill="#E8E8FF"/><rect x=".18" y="1031" width="251.613" height="256" rx="12" fill="#AFC2F2"/></svg>
-
-    );
+<svg width="252" height="2001" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x=".36" y="219" width="251.613" height="190" rx="12" fill="#E8E8FF"/><rect x=".36" y="438" width="251.613" height="355" rx="12" fill="#FFDEDE"/><rect x=".36" width="251.613" height="190" rx="12" fill="#C3E2FF"/><rect y="1674" width="252" height="327" rx="12" fill="#E8E8FF"/><rect x=".36" y="822" width="251.613" height="212" rx="12" fill="#CCD1FF"/><rect y="1308" width="252" height="335" rx="12" fill="#AFC2F2"/><rect x=".36" y="1065" width="251.613" height="212" rx="12" fill="#FFDEDE"/></svg>    );
   }
 }
 
--- src/Asset/SVG/Start_img4.tsx
+++ src/Asset/SVG/Start_img4.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 class Start_img4 extends React.Component {
   render() {
-    return (
-      <svg width="252" height="1889" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="129" width="251.613" height="187" rx="12" fill="#FFDEDE"/><rect width="251.613" height="100" rx="12" fill="#CCD1FF"/><rect y="345" width="251.613" height="256" rx="12" fill="#E8E8FF"/><rect y="630" width="251.613" height="256" rx="12" fill="#AFC2F2"/><rect y="915" width="251.613" height="256" rx="12" fill="#C3E2FF"/><rect y="1593" width="252" height="296" rx="12" fill="#E8E8FF"/><rect y="1200" width="252" height="364" rx="12" fill="#FFDEDE"/></svg>
-    );
+    return ( 
+      <svg width="252" height="2038" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="1872" width="252" height="166" rx="12" fill="#C3E2FF"/><rect y="1316" width="252" height="527" rx="12" fill="#CCD1FF"/><rect x=".388" y="292" width="251.613" height="212" rx="12" fill="#CCD1FF"/><rect x=".388" y="533" width="251.613" height="184" rx="12" fill="#C3E2FF"/><rect x=".388" width="251.613" height="263" rx="12" fill="#FFDEDE"/><rect x=".388" y="746" width="251.613" height="256" rx="12" fill="#E8E8FF"/><rect x=".388" y="1031" width="251.613" height="256" rx="12" fill="#AFC2F2"/></svg>    
+      );
   }
 }
 
