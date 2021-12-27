cbe2018f7462431c1a23e5e7788f1a8f4bf47628
hyeongrok7874 <khr5k0228@gmail.com>
2021-12-27 23:00:58 +0900

    Add banner positioning

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -1,60 +1,12 @@
-import { useSetRecoilState } from "recoil";
-import { VisibleBannerState } from "Atom";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquare as Square } from "@fortawesome/free-regular-svg-icons";
-import * as S from "./style";
-import { useEffect } from "react";
-
-const slideImages = ["banner.png", "banner.png", "banner.png"];
-
-const useVisibleState = () => {
-  const setVisibleBanner = useSetRecoilState(VisibleBannerState);
-
-  const onClick = () => {
-    const date = new Date();
-    date.setDate(date.getDate() + 1);
-    localStorage.setItem("invisible-banner", `${date}`);
-    setVisibleBanner(false);
-  };
-
-  useEffect(() => {
-    const storage = localStorage.getItem("invisible-banner");
-    if (!storage) return;
-    const bannerDate = new Date(`${storage}`);
-    const date = new Date();
-    setVisibleBanner(bannerDate < date);
-  }, []);
-
-  return [onClick];
-};
+import React from "react";
+import * as S from "./style"
 
 const Banner = () => {
-  const [onClick] = useVisibleState();
-
   return (
-    <S.BannerWrapper>
-      <Slide easing="ease">
-        {slideImages.map((slide) => (
-          <div className="each-slide">
-            <div
-              className="slideImage"
-              style={{ backgroundImage: `url(/slideImg/${slide})` }}
-            >
-              <span>배너 IMAGE</span>
-            </div>
-          </div>
-        ))}
-      </Slide>
-      <div className="close">
-        <div onClick={onClick}>
-          <span>오늘 하루 보지 않기</span>
-          <FontAwesomeIcon icon={Square} />
-        </div>
-      </div>
-    </S.BannerWrapper>
-  );
-};
+    <S.BannerBox>
+      
+    </S.BannerBox>
+  )
+}
 
-export default Banner;
+export default Banner
--- src/Components/Banner/style.ts
+++ src/Components/Banner/style.ts
@@ -1,51 +1,10 @@
 import styled from "styled-components";
 import device from "Shared/Config";
 
-export const BannerWrapper = styled.div`
-  position: relative;
-  top: 110px;
-  left: 0;
+export const BannerBox = styled.main`
+  width: 100%;
   height: 120px;
-
-  @media ${device.tablet} {
-    display: none;
-  }
-
-  .slideImage {
-    height: 100px;
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-
-    /* span tag 가운데 정렬 */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .each-slide {
-    height: 100px;
-  }
-  .nav.default-nav {
-    display: none;
-  }
-  .close {
-    background: #6a76e9;
-    height: 30px;
-    width: 100%;
-    color: #fff;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    box-sizing: border-box;
-    padding: 0 20px;
-
-    div {
-      cursor: pointer;
-
-      span {
-        margin-right: 7px;
-      }
-    }
-  }
-`;
+  background-color: blue;
+  position: relative;
+  top: 100px;
+`
