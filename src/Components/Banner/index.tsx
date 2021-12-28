5daa93519953833780e22ab4d955746e0eae2775
hyeongrok7874 <khr5k0228@gmail.com>
2021-12-28 21:04:52 +0900

    Add Banner

--- src/Components/Banner/index.tsx
+++ src/Components/Banner/index.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useSetRecoilState } from "recoil";
+import { VisibleBannerState } from "Atom";
+import BannerContent from "./BannerContent";
 import * as S from "./style"
 
+const useVisibleState = () => {
+  const setVisibleBanner = useSetRecoilState(VisibleBannerState);
+
+  const onClick = () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 1);
+    localStorage.setItem("invisible-banner", `${date}`);
+    setVisibleBanner(false);
+  };
+
+  useEffect(() => {
+    const storage = localStorage.getItem("invisible-banner");
+    if (!storage) return;
+    const bannerDate = new Date(`${storage}`);
+    const date = new Date();
+    setVisibleBanner(bannerDate < date);
+  }, []);
+
+  return [onClick];
+};
+
 const Banner = () => {
+  const [onClick] = useVisibleState();
+
   return (
     <S.BannerBox>
-      
+      <BannerContent Img={`banner1.jpeg`} />
+      <S.InvisibleCheck>
+        <S.CheckButton onClick={onClick}>
+          오늘 하루 보지 않기 
+        </S.CheckButton>
+      </S.InvisibleCheck>
     </S.BannerBox>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
--- src/Components/Banner/style.ts
+++ src/Components/Banner/style.ts
@@ -1,10 +1,32 @@
 import styled from "styled-components";
 import device from "Shared/Config";
 
-export const BannerBox = styled.main`
+export const BannerBox = styled.div`
   width: 100%;
   height: 120px;
-  background-color: blue;
   position: relative;
   top: 100px;
-`
+  margin: 0;
+
+  @media ${device.tablet} {
+    display: none;
+  }
+`;
+
+export const InvisibleCheck = styled.div`
+  height: 16px;
+  background: #6a76e9;
+  padding: 7px 0px;
+`;
+
+export const CheckButton = styled.div`
+  width: 150px;
+  position: absolute;
+  right: 20px;
+  color: white;
+  font-size: 14px;
+  font-weight: 600;
+  :hover{
+    cursor: pointer;
+  }
+`;
