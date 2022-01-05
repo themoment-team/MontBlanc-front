6746f43f12ff473c93bf45e728f4912ceee9f758
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2022-01-05 13:15:01 +0900

    Add CountingStar component

--- src/Components/AboutCounting/index.tsx
+++ src/Components/AboutCounting/index.tsx
@@ -1,7 +1,7 @@
 import Table from "Api/table";
 import { useEffect, useState} from "react";
 import * as S from "./style"
-import useSetInterval from "Hooks/useSetInterval";
+import { CountingStar } from "react-countingstar";
 
 const useAmount = (): [amount: number, date: number] => {
   const [amount, setAmount] = useState({ data: 0 });
@@ -25,25 +25,10 @@ const useAmount = (): [amount: number, date: number] => {
 const AboutCounting = () => {
   const [days, comments] = useAmount();
 
-  const [currentDay, setCurrentDay] = useState(0)
-  const [currentComments, setCurrentComments] = useState(0);
-
-  useSetInterval(() => {
-    if (currentDay !== days) {
-      setCurrentDay(currentDay + 1);
-    }
-  }, 800 / days);
-  
-  useSetInterval(() => {
-    if (currentComments !== comments) {
-      setCurrentComments(currentComments + 1);
-    }
-  }, 800 / comments);
-
   return (
     <S.Counting>
-      <p><span>{currentDay}</span>일 동안</p>
-      <p><span>{currentComments}</span>개의 불편함이</p>
+      <p><CountingStar cntNum={days} time={0.8} fontSize={60}/>&nbsp;일 동안</p>
+      <p><CountingStar cntNum={comments} time={0.8} fontSize={60}/>&nbsp;개의 불편함이</p>
       <p>모였습니다.</p>
     </S.Counting>
   )
